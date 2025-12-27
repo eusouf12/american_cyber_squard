ce340ec1df00ca3f6b6bcc124a4426ac649407f7
debbendu195 <debbendupaul195@gmail.com>
2025-12-28 00:35:46 +0600

    student navbar

--- lib/view/components/custom_nav_bar/student_nav_bar.dart
+++ lib/view/components/custom_nav_bar/student_nav_bar.dart
@@ -1,3 +1,5 @@
+import 'package:america_ayber_squad/view/screen/role_screen/student/view/student_attendance/student_attendance_screen.dart';
+import 'package:america_ayber_squad/view/screen/role_screen/student/view/student_grade_screen/student_grade_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -5,10 +7,8 @@ import 'package:get/get.dart';
 import '../../../utils/app_colors/app_colors.dart';
 import '../../../utils/app_icons/app_icons.dart';
 import '../../screen/role_screen/student/view/student_assignment/student_assignment_screen.dart';
-import '../../screen/role_screen/student/view/student_attendance/student_attendance_screen.dart';
-import '../../screen/role_screen/student/view/student_grade_screen/student_grade_screen.dart';
 import '../../screen/role_screen/student/view/student_home/student_home_screen.dart';
-import '../custom_text/custom_text.dart';
+
 
 class StudentNavBar extends StatefulWidget {
   final int currentIndex;
@@ -16,28 +16,19 @@ class StudentNavBar extends StatefulWidget {
   const StudentNavBar({required this.currentIndex, super.key});
 
   @override
-  State<StudentNavBar> createState() => _StudentNavBarState();
+  State<StudentNavBar> createState() => _UserNavBarState();
 }
 
-class _StudentNavBarState extends State<StudentNavBar> {
+class _UserNavBarState extends State<StudentNavBar> {
   late int bottomNavIndex;
-  final List<String> navbarNameList = [
-    "Dashboard".tr,
-    "Attendance".tr,
-    "Assignment".tr,
-    "Grade".tr,
-  ];
-  final List<String> selectedIcon = [
+
+  final List<String> icons = [
     AppIcons.home,
     AppIcons.attendance,
     AppIcons.assignments,
     AppIcons.grade,
-  ];
-  final List<String> unselectedIcon = [
-    AppIcons.home,
-    AppIcons.attendance,
-    AppIcons.attendance,
-    AppIcons.grade,
+    // AppIcons.navUser,
+
   ];
 
   @override
@@ -48,55 +39,71 @@ class _StudentNavBarState extends State<StudentNavBar> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 20.w),
-      decoration: BoxDecoration(
-          color: Color(0xffFEFEFE),
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20.w),
+        decoration: BoxDecoration(
+          color: AppColors.white,
           borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(20.r),
-            topRight: Radius.circular(20.r),
+            topLeft: Radius.circular(30.r),
+            topRight: Radius.circular(30.r),
           ),
-          border: Border.all(color: AppColors.grey_03,width: .2),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withValues(alpha: 0.1),
-              spreadRadius: 1,
-              blurRadius: 0,
-              offset: const Offset(3, 0),
-            ),
-          ]
-      ),
-      height: 85.h,
-      width: MediaQuery.of(context).size.width,
-      alignment: Alignment.centerLeft,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: List.generate(
-          selectedIcon.length,
-              (index) => InkWell(
-            onTap: () => onTap(index),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                SizedBox(height: 6.h,),
-                Container(
-                  height: 40.h,
-                  width: 40.w,
-                  alignment: Alignment.center,
-                  child: SvgPicture.asset(
-                    index == bottomNavIndex ? selectedIcon[index] : unselectedIcon[index],
-                    height: 24.h,
-                    width: 24.w,
+          border: Border.all(color: AppColors.grey_03, width: .2),
+          // boxShadow: [
+          //   BoxShadow(
+          //     color: Colors.grey,
+          //     spreadRadius: 1,
+          //     blurRadius: 0,
+          //     offset: const Offset(3, 0),
+          //   ),
+          // ],
+        ),
+        height: 80.h,
+        width: MediaQuery.of(context).size.width,
+        alignment: Alignment.centerLeft,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: List.generate(
+            icons.length,
+                (index) => InkWell(
+              onTap: () => onTap(index),
+              borderRadius: BorderRadius.circular(20.r),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(height: 13.h),
+                  Container(
+                    height: 47.62.h,
+                    width: 47.62.w,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      borderRadius: BorderRadius.circular(20.r),
+                      gradient: index == bottomNavIndex
+                          ? const LinearGradient(
+                        colors: [
+                          Color(0xFFFDFFA8),
+                          Color(0xFFC8E6C9),
+                        ],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                      )
+                          : null,
+                      color: index == bottomNavIndex ? null : Colors.transparent,
+                    ),
+                    alignment: Alignment.center,
+                    child: SvgPicture.asset(
+                      icons[index],
+                      height: 25.h,
+                      width: 25.w,
+                      colorFilter: ColorFilter.mode(
+                        index == bottomNavIndex ? Colors.black : const Color(0xFFD2D2D2),
+                        BlendMode.srcIn,
+                      ),
+                    ),
                   ),
-                ),
-                CustomText(
-                  text: navbarNameList[index],
-                  color: index == bottomNavIndex ? AppColors.primary : AppColors.grey_03,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 12.w,
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
@@ -111,18 +118,21 @@ class _StudentNavBarState extends State<StudentNavBar> {
       });
 
       switch (index) {
-      case 0:
-        Get.offAll(() => StudentHomeScreen());
-        break;
-      case 1:
-        Get.to(() => StudentAttendanceScreen());
-        break;
-      case 2:
-        Get.to(() => StudentAssignmentScreen());
-        break;
-      case 3:
-        Get.to(() => StudentGradeScreen());
-        break;
+        case 0:
+          Get.offAll(() => StudentHomeScreen());
+          break;
+        case 1:
+          Get.offAll(() => StudentAttendanceScreen());
+          break;
+        case 2:
+          Get.offAll(() => StudentAssignmentScreen());
+          break;
+        case 3:
+          Get.offAll(() => StudentGradeScreen());
+          break;
+        // case 4:
+        //   Get.to(() => ProfileScreen());
+        //   break;
       }
     }
   }
